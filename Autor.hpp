f36c294ef4cdb8d10d20efe395b62e72617b2c7d
alexarmengou <alex.armengou@gmail.com>
2016-04-26 19:03:04 +0200

    Update Autor.hpp

--- Autor.hpp
+++ Autor.hpp
@@ -1,3 +1,6 @@
+/** @file Autor.hh
+    @brief Especificació de la classe Autor 
+*/
 
 #ifndef Autor_hpp
 #define Autor_hpp
@@ -5,6 +8,8 @@
 #include <string>
 #include <vector>
 
+/** @class Autor
+    @brief Representa un autor i el conjunt dels seus textos */
 class Autor {
 private:
     string nom;
@@ -15,15 +20,55 @@ private:
     int n_paraules;
     
 public:
+  //Constructores 
+
+  /** @brief Creadora per defecte. 
+
+      S'executa automàticament al declarar un autor.
+      \pre <em>cert</em>
+      \post El resultat es un autor sense textos.
+  */ 
     Autor();
+    
+  //Destructores 
     ~Autor();
     
+  //Modificadores
+  
+    /** @brief Afegeix un text al parametre implícit. 
+
+      \pre El paràmetre implícit no conté cap text amb el títol de text.
+      \post El paràmetre implícit conté els textos originals més text.
+  */ 
     void afegir_text(Text text);
+    
+    /** @brief Indica si el parametre implícit conté un text amb un conjunt de paraules. 
+
+      \pre paraules és un conjunt de paraules.
+      \post Si trobat era originalment cert: es manté cert i retorna -1 si existeix algun text al p.i que contingui paraules.
+      Si trobat era originalment fals: trobat es cert si existeix algun text al p.i. que contingui paraules i fals altrament. I retorna la seva posició si aquest es únic i -1 si n'existeix més d'un.
+  */ 
     int busca_triar_text(bool& trobat, const vector<string>& paraules)
-    /* Post: trobat es cert si ha trobat el text que buscavem, fals si no l'ha trobat
-             retorna -1 si error, altrament retorna la posició del vector textos */
+    
+    /** @brief Elimina un text del parametre implícit. 
+
+      \pre El paràmetre implícit conté un text a la posicio pos_text.
+      \post El paràmetre implícit conté els textos originals excepte el text de la posicio pos_text.
+  */     
     void eliminar_text(int pos_text);
+    
+    /** @brief Canvia totes les aparicions d'una paraula per una altra en un text del parametre implícit. 
+
+      \pre El paràmetre implícit conté un text a la posicio pos_text.
+      \post S'han substituït totes les aparicions de par1 per par2 en el text de la posicio pos_text.
+  */      
     void substituir_text(int pos_text, string par1, string par2);
+    
+    /** @brief Escriu el títol de tots els textos del paràmetre implicit. 
+
+      \pre <em>cert</em>
+      \post S'escriuen pel canal estàndard el títol de tots els textos del p.i. ordenats alfabèticament per tí́tol.
+  */      
     void escriure_textos();
     void mostrat_titols();
     void consultar_autor();
